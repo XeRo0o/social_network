813540ef4acbc5d8ffcd1a5d983086d0695f2cb3
William John McCartin III <admin@myxerotek.com>
2011-09-23 12:05:14 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetwork::Application.config.secret_token = '6aa02724c6e4aeb8f0b35f204f06c9666c19f4facc18530edaa8559fa26a8c526bbb995c7b4e46bff6617dc70b4b89da6cbf9b2966663f88361f115434e45f66'
